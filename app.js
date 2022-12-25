a13de39bd8b07f0b6e559a5bcaf29f1e20ad4dc9
Victor Farias <victorfariassb@gmail.com>
2022-12-25 13:48:21 -0300

    melhorias
    criação da última msg, responsividade para telas menores e correção de bug ao mudar o arquivo

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 let node = document.getElementById('output');
-let count_nomes = []
 let count_unique_names = new Set()
-let count_final = {}
+let ol = document.querySelector("ol")
+let ultima_msg = document.querySelector(".ultima-msg")
 
 var openFile = function (event) {
+  count_unique_names.clear()
   // vamos primeiro pegar o arquivo que foi selecionado pelo usuário
   var input = event.target;
   // aqui criamos a função que irá ler o arquivo
@@ -14,6 +15,8 @@ var openFile = function (event) {
     let text = reader.result;
     // separarmos o texto por cada '\n'
     let trechos = text.split('\n')
+    let primeira_msg = trechos[0].split(/ (?:[01]\d|2[0123]):(?:[012345]\d) - /)[0]
+    ultima_msg.innerHTML = "A mensagem mais antiga é de: " + primeira_msg
     count_occurrences(trechos)
     count_string(trechos, count_unique_names)
   };
@@ -21,6 +24,7 @@ var openFile = function (event) {
 };
 
 function count_occurrences(trechos) {
+  let count_nomes = []
   for (let trecho of trechos) {
     // separamos depois da hora e de ' - '  
     trecho = trecho.split(/(?:[01]\d|2[0123]):(?:[012345]\d) - /)
@@ -42,8 +46,11 @@ function count_occurrences(trechos) {
   }, {});
   // depois organizamos da maior para a menor
   count_nomes = Object.fromEntries(Object.entries(count_nomes).sort(([, a], [, b]) => b - a))
-  
   // e criamos a lista no HMTL
+  var child = ol.lastElementChild; 
+  while (child) {
+    ol.removeChild(child);
+    child = ol.lastElementChild;}
   for (let participante in count_nomes) {
     let p = document.createElement('li')
     p.innerHTML = participante + ": " + count_nomes[participante]
@@ -52,6 +59,7 @@ function count_occurrences(trechos) {
 }
 
 function count_string(trechos, participantes){
+  let count_final = {}
   for (let name of participantes) {
     let count_string = 0
     for (let trecho of trechos) {
@@ -71,15 +79,21 @@ function count_string(trechos, participantes){
     count_final[name] = count_string
   }
 
+  var layout = {
+    title: "Nº de caracteres digitados por integrante",
+    showlegend: false,
+  }
+  
+  var config = {responsive: true}
+
   var data = [
     {
-      x: Object.keys(count_final),
-      y: Object.values(count_final),
-      type: 'bar'
+      labels: Object.keys(count_final),
+      values: Object.values(count_final),
+      type: 'pie'
     }
   ];
-  
-  Plotly.newPlot('myDiv', data);
+    Plotly.newPlot('myDiv', data, layout, config);
 }
 
 
